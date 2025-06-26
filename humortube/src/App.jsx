5a6e3ff7bf75f538789cb22583526b484f93be15
SeongminKim-lx <hoemas0921@naver.com>
2025-06-26 11:15:34 +0900

    유머튜브 웹서버에 올림

---
+++ humortube/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const apple = 1
+
+  return (
+    <>
+      <div>
+        <h3>유머튜브</h3>
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Y3nuVjtioAE?si=ndE6-r8e0eb-1OQw&amp;start=43" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Le1JPvEcUuQ?si=BMkpz3NaZLx56Dv6&amp;start=188" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>  
+    </>
+  )
+}
+
+export default App
---
+++ test.js
@@ -0,0 +1,8 @@
+
+let a = 11
+const b = 22
+const c = a + b
+
+a = 50
+
+console.log(c)
